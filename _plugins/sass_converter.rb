38a78d9d03858d63fa5489485dc58915e6ab6d7b
Daryl Fritz <madcowdzz@gmail.com>
2013-12-07 12:32:44 -0500

    Redesign site

--- _plugins/sass_converter.rb
+++ _plugins/sass_converter.rb
@@ -1,14 +1,12 @@
 module Jekyll
   # Sass plugin to convert .scss to .css
-  # 
-  # Note: This is configured to use the new css like syntax available in sass.
   require 'sass'
   class SassConverter < Converter
     safe true
     priority :low
 
      def matches(ext)
-      ext =~ /scss/i
+      ext =~ /\.scss/i
     end
 
     def output_ext(ext)
